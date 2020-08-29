f1ca3c080b7a282069758da23969dd16b50c6a8f
Rafael Sene <rafaelsene@MacBook-Pro.local>
2020-08-29 14:05:35 -0300

    fix: implementação do editor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="col">
+      <editor
+        ref="ace"
+        v-model="crip"
+        @init="editorInit"
+        lang="text"
+        theme="dracula"
+        width="100%"
+        height="100%"
+      />
+      <button @click="criptografar()">Criptografar</button>
+    </div>
+    <div class="col">
+      <editor
+        ref="ace2"
+        v-model="desCrip"
+        @init="editorInit"
+        lang="text"
+        theme="dracula"
+        width="100%"
+        height="100%"
+      />
+      <button>Des Criptografar</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
+  data: () => ({ crip: "", desCrip: "" }),
+  methods: {
+    editorInit() {
+      require("brace/ext/textarea"); //language extension prerequsite...
+      require("brace/mode/text");
+      require("brace/theme/dracula");
+    },
+    criptografar() {
+      // this.desCrip = this.crip;
+    },
+  },
+
   components: {
-    HelloWorld
-  }
+    editor: require("vue2-ace-editor"),
+  },
+
+  mounted() {
+    const editor = this.$refs.ace.editor;
+    const editor2 = this.$refs.ace2.editor;
+    const conf = {
+      fontSize: "18pt",
+    };
+    editor.setOptions(conf);
+    editor2.setOptions(conf);
+  },
 };
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+.col {
+  width: 50%;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+}
+
+.col button {
+  height: 50px;
+  font-size: 20px;
 }
 </style>
